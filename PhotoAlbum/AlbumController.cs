162114abb11394b786b284c49758b6736830503f
Jackie Darnell <jacqueline.d.richter@gmail.com>
2022-04-29 19:10:56 -0500

    Fetch photos by album implemented, test project and methods added
    Added Photo object class
    Added Album controller for asynchronously fetching photos by album id
    Main method contains console read/write
    Updated README.md
    Added .gitignore

---
+++ PhotoAlbum/AlbumController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum
+{
+
+    /// <summary>
+    /// Class for retrieving an album
+    /// </summary>
+    public class AlbumController
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+
+        /// <summary>
+        /// Asyncronously retrieves all photos with given album id
+        /// </summary>
+        /// <param name="albumId">Id of album</param>
+        /// <returns>List containing all photos with the given album id</returns>
+        public static async Task<List<Photo>> FetchAlbum(string albumId)
+        {
+            if (!int.TryParse(albumId, out int input))
+                throw new ArgumentException();
+
+            if (input < 0)
+                throw new ArgumentOutOfRangeException();
+
+            var streamTask = client.GetStreamAsync($@"https://jsonplaceholder.typicode.com/photos?albumId={albumId}");
+            var photos = await JsonSerializer.DeserializeAsync<List<Photo>>(await streamTask);
+
+            return photos;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # LT_PhotoAlbum
+
+Description:	
+Displays the photo ids and titles a chosen album from https://jsonplaceholder.typicode.com/photos
+
+Repository location
+https://github.com/jacquelinedrichter/LT_PhotoAlbum.git
+
+Application specifications:
+Console Application
+Framework: .Net Core 3.1.3
+Language: C#
+
+Building and Running Application
+1) Open PhotoAlbum.sln in Visual Studio.
+2) Under the "Build" toolbar choose "Build Solution" (Ctrl + Shift + B).
+3) Run "PhotoAlbum" in Release mode from the toolbar.
+4) When prompted, enter a value into the command prompt and press "Enter".
+5) If the input is a valid argument the console will return all photo ids 
+   and titles associated with the album id entered.
+
+Notes:
+a) If a non-numeric value is entered the user will be prompted for a number
+b) If the input is less than 0 the user will be informed the argument is out of range
+
+
+
+
+
+
